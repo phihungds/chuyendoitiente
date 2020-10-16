e7bfcaab2c9d496cc86b45ea34bbb977f16a7c94
Phi Hung <phihunds1@gmail.com>
2020-10-16 19:36:51 +0700

    hoanthien

--- trans.js
+++ trans.js
@@ -1,10 +1,14 @@
 let convert = document.getElementById("convert")
 convert.addEventListener('click',trans)
 function trans() {
-    let m0 = document.getElementById("sotien").value
-    let m1 = document.getElementById("money1").value
-    let m2 = document.getElementById("money2").value
-    let dapan = (m0 / m1) * m2
-    let ketqua = "Giá trị chuyển đổi = " + dapan
-    document.getElementById("ketqua").innerText = ketqua
+    let sotiencandoi = document.getElementById("sotien").value
+    let menhgiabandau = document.getElementById("donvibandau").value
+    let menhgiachuyenthanh = document.getElementById("donvichuyenthanh").value
+    let dapan = (sotiencandoi / menhgiabandau) * menhgiachuyenthanh
+    let fromelement = document.getElementById("donvibandau")
+    let from = fromelement.options[fromelement.selectedIndex].innerText
+    let toelement = document.getElementById("donvichuyenthanh")
+    let to = toelement.options[toelement.selectedIndex].innerText
+    let tinnhanhienthi = `${sotiencandoi} ${from} = ${dapan} ${to}`
+    document.getElementById("ketqua").innerText = tinnhanhienthi
 }
